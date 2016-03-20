9c9f9a07d773147ce9a79b4642797d91824dc178
Rodrigo Morais <morais.rm@gmail.com>
2016-03-20 04:16:11 -0300

    Adding sixth chapter.

---
+++ ModelFoldingSignals/Model.elm
@@ -0,0 +1,30 @@
+import Graphics.Element exposing (..)
+import Keyboard
+
+
+type alias Model = Int
+
+initialModel : Model
+initialModel = 0
+
+
+-- UPDATE
+update : a -> Model -> Model
+update event model =
+  model + 1
+
+
+model : Signal Model
+model =
+  Signal.foldp update initialModel Keyboard.presses
+
+
+-- VIEW
+view : Model -> Element
+view model =
+  show model
+
+
+main : Signal Element
+main =
+  Signal.map view model
